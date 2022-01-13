8331cb81cc6df03e2a379e883f3d1b5495a4865b
Tri Pham <tri@marble-d.co.jp>
2022-01-14 03:20:13 +0700

    initial commit

---
+++ helpers/constants.ts
@@ -0,0 +1,6 @@
+export const COUNTRIES = [
+  { key: 'VN', label: 'Việt Nam' }
+];
+
+export const EMAIL_ADDRESS_CONFLICT_ERROR = 'EMAIL_ADDRESS_CONFLICT_ERROR';
+export const ARRANGING_PAYMENT = 'ArrangingPayment';
